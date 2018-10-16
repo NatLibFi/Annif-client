5ebaba4509ccf36e34ff95ca066769df9449f5a4
Osma Suominen <osma.suominen@helsinki.fi>
2018-10-16 11:48:18 +0300

    Add setup.py to enable local install

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='annif_client',
+    version='0.1',
+    url='https://github.com/NatLibFi/Annif-client',
+    author='Osma Suominen',
+    author_email='osma.suominen@helsinki.fi',
+    description='Python client library for accessing Annif REST API',
+    long_description=read('README.md'),
+    py_modules=['annif_client'],
+    install_requires=['requests'])
