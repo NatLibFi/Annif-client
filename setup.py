6d72213ac3ac1955b89fea4b60dca6b716827461
Osma Suominen <osma.suominen@helsinki.fi>
2018-10-24 13:41:02 +0300

    start using bumpversion (define it as a dev dependency in setup.py)

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ def read(fname):
 
 setup(
     name='annif_client',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/NatLibFi/Annif-client',
     author='Osma Suominen',
     author_email='osma.suominen@helsinki.fi',
     description='Python client library for accessing Annif REST API',
     long_description=read('README.md'),
     py_modules=['annif_client'],
-    install_requires=['requests'])
+    install_requires=['requests'],
+    extras_require={
+        'dev': ['bumpversion']
+    })
